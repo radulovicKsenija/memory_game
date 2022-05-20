343280688f41f85be02e1654bdb4eff05a87899d
Xenia Radulovic <ksenija.radulovic@outlook.com>
2022-05-20 17:04:09 +0200

    card and app components added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,116 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import SingleCard from './components/SingleCard';
+
+const cardImages = [
+  { "src": "./images/card-1.jpeg", matched: false },
+  { "src": "./images/card-2.jpeg", matched: false },
+  { "src": "./images/card-3.jpeg", matched: false },
+  { "src": "./images/card-4.jpeg", matched: false },
+  { "src": "./images/card-5.jpeg", matched: false },
+  { "src": "./images/card-6.jpeg", matched: false }
+]
 
 function App() {
+
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(5)
+  const [score, setScore] = useState(0)
+  const [choiceOne, setChoiceOne] = useState(null)
+  const [choiceTwo, setChoiceTwo] = useState(null)
+  const [disabled, setDisabled] = useState(false)
+
+  // shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+    
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setCards(shuffledCards)
+    setTurns(5)
+    setScore(0)
+    setDisabled(false)
+    document.querySelector(".result").innerHTML = "";
+    document.querySelector(".show-results").classList.remove("show");
+  }
+
+  // handle a choice
+  const handleChoice = (card) => {
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+  }
+
+  // compare 2 selected cards 
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      setDisabled(true)
+      if(choiceOne.src === choiceTwo.src) {
+        setCards(prevCards => {
+          return prevCards.map(card => {
+            if(card.src === choiceOne.src) {
+              return {...card, matched: true};
+            }else {
+              return card;
+            }
+          })
+        })
+        setScore(prevScore => prevScore + 1)
+        resetTurn();
+      }else {
+        setTurns(prevTurns => prevTurns - 1);
+        setTimeout(() => resetTurn(), 1000);
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
+
+  // reset choices and increase turn
+  const resetTurn = () => {
+    setChoiceOne(null)
+    setChoiceTwo(null)
+    setDisabled(false)
+  }
+
+  // start the game automatically
+  useEffect(() => {
+    shuffleCards();
+  }, []);
+
+  // show game over if the player loses
+  useEffect(() => {
+    if(turns <= 0) {
+      setDisabled(true);
+      document.querySelector(".result").innerHTML = "Game Over";
+      document.querySelector(".show-results").classList.add("show");
+    }
+  }, [turns, disabled])
+
+  // show congratulations if player wins
+
   return (
     <div className="App">
-      <h1>Memory Game</h1>
-      <button>New Game</button>
+      <div className='header'>
+        <p>Turns: {turns}</p>
+        <h1>Memory Game</h1>
+        <p>Score: {score}</p>
+      </div>
+
+      <div className='card-grid'>
+        {cards.map(card => (
+          <SingleCard 
+            key={card.id} 
+            card={card}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+            disabled={disabled}
+          />
+        ))}
+      </div>
+      <div className='show-results'>
+        <p className='result'></p>
+        <button onClick={shuffleCards}>New Game</button>
+      </div>
     </div>
   );
 }
